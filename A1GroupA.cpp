ab1f8cfbe9ce0bfcf640cdcdaad2091b3f9a0c48
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-04-29 21:47:27 +0530

    Update A1GroupA.cpp

--- A1GroupA.cpp
+++ A1GroupA.cpp
@@ -1,5 +1,8 @@
 //Problem Statement:
 
+/*Consider telephone book database of N clients. Make use of a hash table implementation to quickly look up client‘s telephone Number. 
+Make Use of two Collision Techniques and Compare them using number of Comparisons required to find a set of telephone numbers.*/
+
 //Code:
 
 #include<iostream>
